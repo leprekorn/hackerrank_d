838d6b9103e686691c1e415c52f870ac6c5d3274
leprekorn <leprekorn@yandex.ru>
2023-02-14 09:25:19 +0500

    Fix review issues

--- 1.sh
+++ 1.sh
@@ -1,11 +1,23 @@
-#!/bin/bash
-git config --global user.name hackerrank
-git config --global user.email me@hackerrank.com
-cd /var/save
+# Install Git
+sudo apt-get update
+sudo apt-get install git
+
+# Configure the user
+git config --global user.name "hackerrank"
+git config --global user.email "me@hackerrank.com"
+
+# Initialize the repository
+cd /var/save/
 mkdir my-repo
-touch .gitignore
+cd my-repo/
 git init
-git add .
-git commit -m “my first commit”
-git remote add origin /var/save/my-repo.git
-git push -set-upstream origin master
+git remote add origin .
+
+# Check out the master branch and create a commit
+git checkout -b master
+echo "Initial commit" > README.md
+git add README.md
+git commit -m "my first commit"
+
+# Push the changes to the master branch
+git push -u origin master
